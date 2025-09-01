cd8bcdec94609b90161586377e6324aaaa50d0fa
Ramil Musin <musin@flarian.ru>
2025-09-01 17:52:07 +0300

    feat(EsLint): :wrench: add typescript-eslint plugin and reconfigurate rules
    add typescript-eslint. fix import-sort rules, fix json rules, fix stylistic rules

--- eslint/rules/import-sort.mjs
+++ eslint/rules/import-sort.mjs
@@ -2,7 +2,16 @@ import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
 /** @type {import('eslint').Linter.Config} */
 export default {
-	plugins: { '@simple-import-sort': simpleImportSort },
+	files: [
+		'**/*.js',
+		'**/*.jsx',
+		'**/*.ts',
+		'**/*.tsx',
+		'**/*.vue',
+	],
+	plugins: {
+		'@simple-import-sort': simpleImportSort,
+	},
 	rules: {
 		'@simple-import-sort/imports': [
 			'warn',
