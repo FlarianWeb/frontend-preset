919dd5ef7f8c2868f216726fe45471e590ec944f
Ramil Musin <musin@flarian.ru>
2026-01-08 16:52:25 +0300

    fix(Stylelint): :bug: Remove require.resolve for plugin paths to support modern Node.js

--- src/stylelint/config/less.ts
+++ src/stylelint/config/less.ts
@@ -1,24 +1,12 @@
-import { createRequire } from 'module';
 import { Config } from 'stylelint';
 
 import baseRules from './rules/base';
 import orderRules from './rules/order';
 import orderGroupsRules from './rules/order-groups';
 
-const require = createRequire(import.meta.url);
-const stylelintPlugins = {
-	order: require.resolve('stylelint-order'),
-	less: require.resolve('stylelint-less'),
-};
-const stylelintConfig = {
-	standard: require.resolve('stylelint-config-standard'),
-	standardLess: require.resolve('stylelint-config-standard-less'),
-	recommendedVue: require.resolve('stylelint-config-recommended-vue'),
-};
-
 export const lessConfig: Config = {
-	plugins: [stylelintPlugins.order, stylelintPlugins.less],
-	extends: [stylelintConfig.standard, stylelintConfig.standardLess, stylelintConfig.recommendedVue],
+	plugins: ['stylelint-order', 'stylelint-less'],
+	extends: ['stylelint-config-standard', 'stylelint-config-standard-less', 'stylelint-config-recommended-vue'],
 	rules: {
 		...baseRules,
 		...orderRules,
--- src/stylelint/config/scss.ts
+++ src/stylelint/config/scss.ts
@@ -1,24 +1,12 @@
-import { createRequire } from 'module';
 import { Config } from 'stylelint';
 
 import baseRules from './rules/base';
 import orderRules from './rules/order';
 import orderGroupsRules from './rules/order-groups';
 
-const require = createRequire(import.meta.url);
-const stylelintPlugins = {
-	order: require.resolve('stylelint-order'),
-	scss: require.resolve('stylelint-scss'),
-};
-const stylelintConfig = {
-	standard: require.resolve('stylelint-config-standard'),
-	standardScss: require.resolve('stylelint-config-standard-scss'),
-	recommendedVue: require.resolve('stylelint-config-recommended-vue'),
-};
-
 export const scssConfig: Config = {
-	plugins: [stylelintPlugins.order, stylelintPlugins.scss],
-	extends: [stylelintConfig.standard, stylelintConfig.standardScss, stylelintConfig.recommendedVue],
+	plugins: ['stylelint-order', 'stylelint-scss'],
+	extends: ['stylelint-config-standard', 'stylelint-config-standard-scss', 'stylelint-config-recommended-vue'],
 	rules: {
 		...baseRules,
 		...orderRules,
